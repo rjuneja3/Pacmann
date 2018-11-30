8784fdfe05fbb314eb98ec661c0f5753c9bc49a7
Rohan Juneja <rjuneja3@my.centennialcollege.ca>
2018-11-30 15:22:55 -0400

    NewNew
    firstattempt

---
+++ Assets/Scripts/EatPacdot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EatPacdot : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "pacmann")
+            Destroy(gameObject);
+    }
+}
